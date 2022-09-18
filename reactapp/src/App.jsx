e979c50a84a9c5ea63d15a3fc866195f480667e3
fukudayusuke <fukudayusuke.fy.@gmail.com>
2022-09-18 18:10:54 +0900

    hellow would

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => 
+{
+    // return null;
+
+    const onClickButton = () => 
+    {
+        alert();
+    };
+
+    return (
+        <>
+        <h1>hellow would</h1>
+        <p>Media 1</p>
+        <button onClick={onClickButton}>Button</button>
+        </>
+    )
+};
