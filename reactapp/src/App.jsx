8a14fc36cf3f62831ff9d56e81291585dc05d9a5
fukudayusuke <fukudayusuke.fy.@gmail.com>
2022-10-18 01:08:25 +0900

    state

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./CssModules";
+
 export const App = () => 
 {
     // return null;
 
+    const [ num, setNum ] = useState(0);
+
+    useEffect( () => {
+        alert();
+    }, [num] )
+
     const onClickButton = () => 
     {
-        alert();
+        // alert();
+        setNum( num+1 );
     };
+
 
     return (
         <>
-        <h1>hellow would</h1>
-        <p>Media 1</p>
-        <button onClick={onClickButton}>Button</button>
+        {/* <h1 style= {{ color : "red" }} > hellow would </h1> */}
+        {/* <ColoredMessage color="blue" message="Media1" /> */}
+        {/* <ColoredMessage color="pink" message="Media2" /> */}
+        {/* <ColoredMessage > fukuda </ColoredMessage > */}
+        {/* <button onClick={onClickButton}>Button</button> */}
+        {/* <p>{num}</p> */}
+        <CssModules></CssModules>
         </>
-    )
+    );
 };
